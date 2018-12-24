5024afea5631d3e3fb277f9331fe75e9f19f096c
Isaiah Nields <imnields@gmail.com>
2018-12-24 13:21:32 -0500

    Update 607. Sales Person.sql

--- leetcode/607. Sales Person.sql
+++ leetcode/607. Sales Person.sql
@@ -1,6 +1,7 @@
-SELECT salesperson.name FROM salesperson
-WHERE salesperson.sales_id NOT IN 
-    (SELECT DISTINCT o.sales_id 
+# Write your MySQL query statement below
+SELECT name FROM salesperson
+WHERE sales_id NOT IN 
+    (SELECT o.sales_id 
     FROM company c 
     INNER JOIN orders o ON o.com_id = c.com_id 
     WHERE c.name = "RED");
